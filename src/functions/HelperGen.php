be7ad9a9f10ca109d3c8f58d15adaf3c6418b3e0
ahmed <alfker3@gmail.com>
2025-07-28 23:49:40 +0300

    Update docblock for Youtube method and fix return link for non-iframe option

--- src/functions/HelperGen.php
+++ src/functions/HelperGen.php
@@ -12,7 +12,8 @@ class HelperGen
      * @param int $width
      * @param int $height
      * @param string $title
-     * @return string
+     * @param bool $iframe 'Whether to return an iframe or just the YouTube link'
+     * @return string 
      */
 
     public static function Youtube($link, $width = 560, $height = 315,$title = 'YouTube video player',$iframe = true)
@@ -23,7 +24,7 @@ class HelperGen
         if($iframe) {
             return '<iframe width="'.$width.'" height="'.$height.'" src="https://www.youtube.com/embed/'.$youtube_id.'" title="'.$title.'" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>';
         }else{
-            return "https://www.youtube.com/embed/" . $youtube_id; // This will return the YouTube embed link only
+            return "https://www.youtube.com/watch?v=" . $youtube_id; // This will return the YouTube embed link only
         }
 
 
